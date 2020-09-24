f65d243acc76fe9b57ffb25c5e4e642b88fb278c
Mike Owens <mocoding86@gmail.com>
2020-09-23 20:47:32 -0700

    figured out how to wait for movie id array to populate with setInterval function in order to use more api calls for more information

--- js/index.js
+++ js/index.js
@@ -24,33 +24,72 @@ const topMovieIds = [];
 
 
 /** Fetch Top Movies */
-const topMoviesApi = () => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&language=en-US&sort_by=popularity.desc&page=1`)
-    .then((res) => res.json())
-    .then((data) => {
-        const movies = data.results.slice(0,6);
-        // console.log(movies);
-        movies.forEach(movie => {
-            topMovieIds.push(movie.id);
-            let output = `
-                <div class="col-sm-4">
-                    <div class="card m-2 card-zoom">
-                        <a href="#">
-                            <img class="card-img-top" src="//image.tmdb.org/t/p/w440_and_h660_face/${movie.poster_path}" alt="" title="${movie.original_title}">
-                        </a>
-                        <div class="card-body">
-                            <h6 class="card-title text-center">${movie.original_title}</h6>
-                        </div>
+// const fetchTopMovies = () => {
+//     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&language=en-US&sort_by=popularity.desc&page=1`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         const movies = data.results.slice(0,6);
+//         // console.log(movies);
+//         movies.forEach(movie => {
+//             topMovieIds.push(movie.id);
+//             let output = `
+//                 <div class="col-sm-4">
+//                     <div class="card m-2 card-zoom">
+//                         <a href="#">
+//                             <img class="card-img-top" src="//image.tmdb.org/t/p/w440_and_h660_face/${movie.poster_path}" alt="" title="${movie.original_title}">
+//                         </a>
+//                         <div class="card-body">
+//                             <h6 class="card-title text-center">${movie.original_title}</h6>
+//                         </div>
+//                     </div>
+//                 </div>
+//             `;
+//             topMovies.innerHTML += output;
+//         });
+//     })
+//     .catch((err) => console.log(err))
+// }
+
+async function fetchTopMovies() {
+    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${tmdbKey}&language=en-US&sort_by=popularity.desc&page=1`);
+    const movies = await response.json();
+    return movies;
+};
+
+fetchTopMovies().then(movies => {
+    const slicedMovies = movies.results.slice(0,6);
+    slicedMovies.forEach(movie => {
+        topMovieIds.push(movie.id);
+        let output = `
+            <div class="col-sm-4">
+                <div class="card m-2 card-zoom">
+                    <a href="#">
+                        <img class="card-img-top" src="//image.tmdb.org/t/p/w440_and_h660_face/${movie.poster_path}" alt="" title="${movie.original_title}">
+                    </a>
+                    <div class="card-body">
+                        <h6 class="card-title text-center">${movie.original_title}</h6>
                     </div>
                 </div>
-            `;
-            topMovies.innerHTML += output;
-        });
-    })
-    .catch((err) => console.log(err))
+            </div>
+        `;
+        topMovies.innerHTML += output;
+    });
+});
+
+fetchTopMovies().catch((err) => console.log(err));
+
+const waitForAPI = () => {
+    const timeout = setInterval(() => {
+        if (topMovieIds.length === 6) {
+            // console.log(topMovieIds[0]);
+            clearInterval(timeout);
+        }
+    }, 100);
 }
 
-topMoviesApi();
+waitForAPI();
+
+
 
 /** Steps for getting movie info 
  * Try another api call
@@ -58,7 +97,7 @@ topMoviesApi();
  * Incorporate the info with the a href in the output and try it
 */
 
-console.log(topMovieIds[2]);
+
 
 // const topMoviesInfo = () => {
 //     fetch(`https://api.themoviedb.org/3/movie/${topMovieIds.map(movieId => movieId)}?api_key=${tmdbKey}&language=en-US`)
